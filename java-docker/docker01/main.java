a41be20a7c0a1ff347ec4cbae09090f081b07099
Supratim Nag <snsupratim@gmail.com>
2024-12-27 10:57:03 +0530

    docker-java01

---
+++ java-docker/docker01/main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello,Docker! Current date:" + currentDate);
+    }
+}
